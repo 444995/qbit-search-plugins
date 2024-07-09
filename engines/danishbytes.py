7bbca92eb56b762e811d0fc8d92004fb89b1b471
oliver748 <olivereksd@proton.me>
2024-07-09 23:21:08 +0200

    danishbytes update

--- engines/danishbytes.py
+++ engines/danishbytes.py
@@ -1,15 +1,30 @@
-#VERSION: 1.00
+# VERSION: 1.00
 # AUTHORS: github.com/444995 - the code is bad right now, i know
 # LICENSING INFORMATION
+
+# ----------------USER SETTINGS-------------------------- #
+
+PRIVATE_USERNAME = "REPLACE_ME"                           # A DANISHBYTES ACCOUNT HAS A PRIVATE USERNAME
+PUBLIC_USERNAME = "REPLACE_ME"                            # AND A PUBLIC USERNAME
+PASSWORD = "REPLACE_ME"                                   # YOU ALSO NEED A PASSWORD
+CACHE_LOGIN = True                                        # THIS IS TO CACHE THE LOGIN SESSION, SO YOU DON'T HAVE TO LOGIN EVERY TIME
+LOGIN_SESSION_PATH = "danishbytes.pkl"                    # THE PATH TO THE LOGIN SESSION FILE
+
+# ----------------NECESSARY IMPORTS---------------------- #
 
 from html.parser import HTMLParser
 from helpers import download_file, retrieve_url
 from novaprinter import prettyPrinter
 
-# some other imports if necessary
+# ----------------IMPORTS-------------------------------- #
+
 import requests
 import pickle
+import os
 from bs4 import BeautifulSoup
+import urllib.parse
+
+# ------------------------------------------------------- #
 
 class danishbytes(object):
     """
@@ -36,8 +51,114 @@ class danishbytes(object):
 
     def __init__(self):
         """
-        Some initialization
+        Setting up env
         """
+        self.base_url = 'https://danishbytes.club/'
+        self.login_url = 'https://danishbytes.club/login'
+        self.user_agent = "Mozilla/5.0 (Linux; Android 10; RMX2185) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Mobile Safari/537.36"
+        self.login_headers = {
+            'content-type': 'application/x-www-form-urlencoded',
+            'user-agent': self.user_agent
+        }
+        self.cur_dir = os.path.dirname(os.path.realpath(__file__))
+
+        self.session = self._handle_user_login()
+
+    def _get_db_session(self, response):
+        """
+        Extracts the db_session from the response headers.
+        """
+        return str(str(response.headers).split("db_session=")[1].split(";")[0]).strip()
+    
+    def _get_xsrf_token(self, response):
+        """
+        Extracts the XSRF token from the response headers.
+        """
+        return str(str(response.headers).split("XSRF-TOKEN=")[1].split(";")[0]).strip()
+
+    def _write_cache_to_file(self, session):
+        """
+        Writes the session cache to a file.
+        """
+        with open(os.path.join(self.cur_dir, LOGIN_SESSION_PATH), 'wb') as file:
+            pickle.dump(session.cookies, file)
+        
+    def _read_cache_from_file(self):
+        """
+        Reads the session cache from a file.
+        """
+        with open(os.path.join(self.cur_dir, LOGIN_SESSION_PATH), 'rb') as file:
+            session = requests.Session()
+            session.cookies.update(pickle.load(file))
+            return session
+
+    def _extract_meta_content(self, soup, name):
+        """Extracts content from meta tag with the given name."""
+        tag = soup.find("meta", {"name": name})
+        return tag["content"].strip() if tag else None
+    
+    def _extract_input_value(self, soup, name):
+        """Extracts value from input tag with the given name."""
+        tag = soup.find("input", {"name": name})
+        return tag["value"].strip() if tag else None
+
+    def _extract_dynamic_name_and_value(self, soup):
+        """Extracts dynamic input name and value after the username input."""
+        dynamic_name, dynamic_value = None, None
+        for i, element in enumerate(soup.find_all("input", {"name": True, "value": True})):
+            name = element["name"]
+            if name == "_username":
+                dynamic_name = soup.find_all("input", {"name": True, "value": True})[i+1]["name"]
+                dynamic_value = soup.find_all("input", {"name": True, "value": True})[i+1]["value"]
+                return dynamic_name, dynamic_value
+
+    def _handle_user_login(self):
+        """
+        This function is used to handle the user login
+        """
+        if CACHE_LOGIN and os.path.exists(LOGIN_SESSION_PATH):
+            return self._read_cache_from_file()
+            
+        main_page = requests.get(self.base_url, allow_redirects=False)
+        initial_db_session = self._get_db_session(main_page)
+
+        headers = {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+            'cookie': f'db_session={initial_db_session}',
+            'user-agent': self.user_agent
+        }
+
+        response = requests.get(self.login_url, headers=headers, allow_redirects=False)
+
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        csrf_token = self._extract_meta_content(soup, "csrf-token")
+        captcha_key = self._extract_input_value(soup, "_captcha")
+        dynamic_name, dynamic_value = self._extract_dynamic_name_and_value(soup)
+        _username = self._extract_input_value(soup, "_username")
+        xsrf_token = self._get_xsrf_token(response)
+        db_session = self._get_db_session(response)
+
+        self.login_headers['cookie'] = f'XSRF-TOKEN={xsrf_token}; db_session={db_session}'
+
+        payload = {
+            "_token": csrf_token,
+            "private_username": PRIVATE_USERNAME,
+            "username": PUBLIC_USERNAME,
+            "password": PASSWORD,
+            "remember": "on",
+            "_captcha": captcha_key,
+            "_username": _username,
+            dynamic_name: dynamic_value
+        }
+        
+        session = requests.Session()
+        login_response = session.post(self.login_url, headers=self.login_headers, data=urllib.parse.urlencode(payload), allow_redirects=True)
+
+        if CACHE_LOGIN:
+            self._write_cache_to_file(session)
+        
+        return session
 
     def download_torrent(self, info):
         """
@@ -59,23 +180,12 @@ class danishbytes(object):
         `what` is a string with the search tokens, already escaped (e.g. "Ubuntu+Linux")
         `cat` is the name of a search category in ('all', 'anime', 'books', 'games', 'movies', 'music', 'pictures', 'software', 'tv')
         """
-        # code will be added so it automatically logs in and uses that session (instead of this hardcoded one)
-        SESSION_FILE = 'c:/Users/Admin/Desktop/db/1session.pkl'
 
-
-        def load_session(file_path):
-            session = requests.Session()
-            with open(file_path, 'rb') as f:
-                session.cookies.update(pickle.load(f))
-            return session
-
-        session = load_session(SESSION_FILE)
-
-
-        response = session.get("https://danishbytes.club/")
+        response = self.session.get(self.base_url)
 
         soup = BeautifulSoup(response.text, "html.parser")
-        csrf_token = soup.find("meta", {"name": "csrf-token"})["content"].strip()
+
+        csrf_token = self._extract_meta_content(soup, "csrf-token")
 
         def get_info(search_results):
             rss_key = search_results['rsskey']
@@ -103,7 +213,6 @@ class danishbytes(object):
                 engine_url = f"https://danishbytes.club"
                 desc_url = f"https://danishbytes.club/torrent/{torrent['id']}"
 
-                # print link|name|size|seeds|leech|engine_url|desc_link
                 print(f"{magnet_url}|{name}|{size}|{seeders}|{leechers}|{engine_url}|{desc_url}")
 
                 torrent_num += 1
@@ -116,9 +225,13 @@ class danishbytes(object):
 
             req_url = f"https://danishbytes.club/torrents/filter?_token={csrf_token}&search={what}&search_not=&uploader=&imdb=&tvdb=&view=list&tmdb=&mal=&igdb=&size_min=0&size_max=0&year_min=&year_max=&categories%5B%5D=1&categories%5B%5D=2&categories%5B%5D=5&categories%5B%5D=4&categories%5B%5D=3&categories%5B%5D=8&types%5B%5D=34&types%5B%5D=30&types%5B%5D=1&types%5B%5D=2&types%5B%5D=3&types%5B%5D=4&types%5B%5D=5&types%5B%5D=6&types%5B%5D=33&types%5B%5D=7&types%5B%5D=8&types%5B%5D=9&types%5B%5D=10&types%5B%5D=19&types%5B%5D=14&types%5B%5D=16&types%5B%5D=17&types%5B%5D=18&types%5B%5D=11&types%5B%5D=20&types%5B%5D=21&types%5B%5D=22&types%5B%5D=12&types%5B%5D=13&types%5B%5D=23&types%5B%5D=24&types%5B%5D=25&types%5B%5D=26&types%5B%5D=27&types%5B%5D=28&types%5B%5D=29&types%5B%5D=31&types%5B%5D=32&types%5B%5D=35&types%5B%5D=15&types%5B%5D=36&types%5B%5D=37&resolutions%5B%5D=1&resolutions%5B%5D=2&resolutions%5B%5D=3&resolutions%5B%5D=4&resolutions%5B%5D=5&resolutions%5B%5D=6&resolutions%5B%5D=7&resolutions%5B%5D=8&resolutions%5B%5D=9&resolutions%5B%5D=10&resolutions%5B%5D=11&language_codes%5B%5D=gb&language_codes%5B%5D=dk&language_codes%5B%5D=xx&language_codes%5B%5D=se&language_codes%5B%5D=no&language_codes%5B%5D=fi&language_codes%5B%5D=jp&language_codes%5B%5D=fr&language_codes%5B%5D=es&language_codes%5B%5D=de&language_codes%5B%5D=po&language_codes%5B%5D=kr&language_codes%5B%5D=is&language_codes%5B%5D=it&language_codes%5B%5D=pt&language_codes%5B%5D=ru&language_codes%5B%5D=cn&language_codes%5B%5D=nl&language_codes%5B%5D=ae&language_codes%5B%5D=in&language_codes%5B%5D=tu&language_codes%5B%5D=th&language_codes%5B%5D=gr&language_codes%5B%5D=ro&language_codes%5B%5D=ba&language_codes%5B%5D=id&language_codes%5B%5D=hu&language_codes%5B%5D=ir&language_codes%5B%5D=ua&language_codes_subs%5B%5D=gb&language_codes_subs%5B%5D=dk&language_codes_subs%5B%5D=xx&language_codes_subs%5B%5D=se&language_codes_subs%5B%5D=no&language_codes_subs%5B%5D=fi&language_codes_subs%5B%5D=jp&language_codes_subs%5B%5D=fr&language_codes_subs%5B%5D=es&language_codes_subs%5B%5D=de&language_codes_subs%5B%5D=po&language_codes_subs%5B%5D=kr&language_codes_subs%5B%5D=is&language_codes_subs%5B%5D=it&language_codes_subs%5B%5D=pt&language_codes_subs%5B%5D=ru&language_codes_subs%5B%5D=cn&language_codes_subs%5B%5D=nl&language_codes_subs%5B%5D=ae&language_codes_subs%5B%5D=in&language_codes_subs%5B%5D=tu&language_codes_subs%5B%5D=th&language_codes_subs%5B%5D=gr&language_codes_subs%5B%5D=ro&language_codes_subs%5B%5D=ba&language_codes_subs%5B%5D=id&language_codes_subs%5B%5D=hu&language_codes_subs%5B%5D=ir&language_codes_subs%5B%5D=ua&page={page_num}&qty=100"
 
-            search_results = session.get(req_url).json()
+            search_results = self.session.get(req_url).json()
 
             torrent_num = get_info(search_results)
 
             if torrent_num < 100:
                 break
+
+if __name__ == "__main__":
+    engine = danishbytes()
+    engine.search("hello")
